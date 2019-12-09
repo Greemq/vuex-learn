3039dcaee40f4324274521ac244a279dc4d4db20
greemv <hvitl@mail.ru>
2019-12-10 00:10:46 +0600

    Initial commit

---
+++ src/components/ProductList.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <h1>Product List</h1>
+
+    <img v-if="loading"
+         src="https://i.imgur.com/JfPpwOA.gif" alt="">
+    <ul v-else>
+      <li v-for="product in products">
+        {{product.title}}-{{product.price | currency}} - {{product.inventory}}
+        <button :disabled="!productIsInStock(product)"
+                @click="addProductToCart(product)">Add to cart
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+    import {mapState,mapGetters,mapActions} from 'vuex'
+    export default {
+        data() {
+            return {
+                loading: false,
+                productIndex:1
+            }
+        },
+        // computed:mapState({
+        //     products:state=>state.products
+        // }),
+        computed: {
+            ...mapState({
+               products:state=>state.products.items
+            }),
+            ...mapGetters('products',{
+               productIsInStock:'productIsInStock'
+            }),
+        }
+        ,
+        methods: {
+            ...mapActions({
+                fetchProducts:'products/fetchProducts',
+                addProductToCart:'cart/addProductToCart'
+            }),
+        },
+        created() {
+            // shop.getProducts(products => {
+            //     // store.state.products = products
+            //     store.commit('setProducts',products)
+            //
+            // })
+            this.loading = true;
+            this.fetchProducts()
+                .then(() => this.loading = false)
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/store/actions.js
@@ -0,0 +1,24 @@
+import shop from "@/api/shop"
+
+
+export default {//=methods
+  // fetchProducts({commit}) {
+  //   return new Promise((resolve, reject) => {
+  //     shop.getProducts(products => {
+  //       commit('setProducts', products)
+  //       resolve()
+  //     })
+  //   })
+  //
+  //   //make the call
+  //   //run setProducts mutation
+  //   // context.commit()
+  // },
+  // addToCart(context,product){
+  //   if (product.inventory>0){
+  //     context.commit('pushProductToCart',product)
+  //   }
+  // }
+
+}
+
